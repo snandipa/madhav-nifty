2bb04ef539834409fbfb7138b3ce0107680d59df
snandipa <mnandipa@gmail.com>
2012-03-04 14:00:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadhavNifty::Application.config.secret_token = '686887bb21f79c03817c8bf6fa207c0f4f900ce7c921e521c6eef917bc77d226f41153e0f22f3f80b232871a1ac71c9e1c4fd42f25cf0e0b1974ddefac834312'
